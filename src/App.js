4945805b61ae3f5ea78182f5d7f9f6f843e33d54
ZionPraise <binarycoder.com@gmail.com>
2022-07-21 21:26:20 +0100

    cleanup1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import React from "react";
+import "./index.css"
 
 function App() {
   return (
-    <div className="App">
+    <div>
      <h1>hello world</h1>
     </div>
   );
