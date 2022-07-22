4f8f056d6a7b588157fa6c6d8c06b8dfb93027c9
ZionPraise <binarycoder.com@gmail.com>
2022-07-22 10:46:25 +0100

    hero-img

---
+++ src/components/Heroimg.js
@@ -0,0 +1,15 @@
+import "./Heroimgstyles.css"
+
+import React from 'react'
+
+const Heroimg = () => {
+  return (
+    <div className="hero">
+        <div className="mask">
+            <img className="hero-img" />
+        </div>
+    </div>
+  )
+}
+
+export default Heroimg
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Heroimg from '../components/Heroimg'
 import Navbar from '../components/Navbar'
 
 function Home() {
   return (
     <div>
         <Navbar />
+        <Heroimg />
     </div>
   )
 }
